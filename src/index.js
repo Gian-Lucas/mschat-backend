666bc76b304a250b8ff0cc06216b6605f80a8703
= <=>
2022-05-09 12:14:45 -0300

    add socket.io

--- src/index.js
+++ src/index.js
@@ -4,8 +4,15 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 
+const { Server } = require("socket.io");
+const io = new Server(server);
+
 app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
+});
+
+io.on("connection", (socket) => {
+  console.log("user connected");
 });
 
 server.listen(process.env.PORT || 8080, () => {
