493b06d879cc718d4ce2c02edd1f89a1a2679db4
= <=>
2022-05-10 15:02:27 -0300

    connection and disconnect socket

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
 
 const http = require("http");
 const server = http.createServer(app);
@@ -7,16 +10,31 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: process.env.CORS_ORIGIN || "http://localhost:3000",
+    origin: process.env.ORIGIN_URL || "http://localhost:3000",
   },
 });
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
-});
+const users = [];
 
 io.on("connection", (socket) => {
-  console.log("user connected");
+  console.log(`user ${socket.id} connected`);
+  socket.broadcast.emit("enter", `usuário ${socket.id} entrou`);
+
+  users.push(socket.id);
+
+  console.log(users);
+
+  socket.on("disconnect", () => {
+    for (let i = 0; i < users.length; i++) {
+      if (users[i] === socket.id) {
+        users.splice(i, 1);
+      }
+    }
+    console.log(`user ${socket.id} disconnected`);
+    socket.broadcast.emit("exit", `usuário ${socket.id} saiu`);
+
+    console.log(users);
+  });
 });
 
 server.listen(process.env.PORT || 8080, () => {
